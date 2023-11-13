d23680096ae267f8be562c4ce64e54e77e09f178
sammyjh30 <sammyjh30@gmail.com>
2023-11-13 23:57:47 +0200

    Section-4: Day 3, video 33. Bonus lesson on for-loops

---
+++ app1/bonus/bonus_3.py
@@ -0,0 +1,9 @@
+meals = ['pasta', 'pizza', 'salad']
+
+for meal in meals:              # meal is a new variable created on the fly. => meal = 'pasta', meal = 'pizza', meal = 'salad'
+    print(meal.capitalize())
+
+for meal in 'meals':        # will print each character
+    print(meal)
+
+print('Bye!')
