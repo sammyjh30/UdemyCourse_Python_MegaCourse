c22dc6e06a132ad276497a7add90c83363b927b6
sammyjh30 <sammyjh30@gmail.com>
2023-11-26 00:01:42 +0200

    Section-5: Day 4, video 40. Editing value in todo list

--- app1/main.py
+++ app1/main.py
@@ -1,17 +1,22 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show' | 'display':            # bitwise "or"
+        case 'show':
             for item in todos:
-                item = item.title()     # Can have multiple lines, must make sure indentation is consistent
+                item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))   # Input always outputs a string
+            number -= number
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'exit':
             break
         case _:      # Doesn't need to be defined prior. Can be variable, standard practice uses _
