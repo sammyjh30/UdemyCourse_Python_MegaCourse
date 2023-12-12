cb62a10ff68c6f58c838699d9181827593a36f4c
sammyjh30 <sammyjh30@gmail.com>
2023-12-13 00:06:17 +0200

    Section-10: Day 9, video 92. Using dictionaries and all()

--- app1/bonus/bonus_9.py
+++ app1/bonus/bonus_9.py
@@ -1,18 +1,28 @@
 
 password = input("Enter new password: ")
-result = []
+#  Dictionaries have a key-value pair e.g.{"key1":"value1", "key2":"value2"}
+# Fetch the value using the key e.g dict["key1"]
+result = {}
 
-if len(password) >= 0:
-    result.append(True)
+if len(password) >= 8:
+    result["length"] = True
 else:
-    result.append(False)
+    result["length"] = False
 
 digit = False
+uppercase = False
 for i in password:
     if i.isdigit():
         digit = True
+    elif i.isupper():
+        uppercase = True
 
-if digit:
-    result.append(True)
+result["digit"] = digit
+result["uppercase"] = uppercase
+
+if all(result.values()):    # unless you specify values, all() checks the keys and will give True even if a Value is false
+    print("Strong Password")
 else:
-    result.append(False)
+    print("Weak Password")
+
+print(result)
